87177268b511aac63a1bfc92d9ba49ff0d95b24e
jys9049 <98201515+jys9049@users.noreply.github.com>
2022-11-16 19:19:43 +0900

    Update employmentsApi.js

--- src/apis/employmentsApi.js
+++ src/apis/employmentsApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { PROXY } from './proxy'
 
 const getEmployments = async (page, size) => {
-  const res = await axios.get(`/employments?page=${page}&size=${size}`)
+  const res = await axios.get(`${PROXY}/employments?page=${page}&size=${size}`)
 
   return res
 }
